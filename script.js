48d9bacbb91c95ce5bb12079eb5e5b512cea3f6a
Littlezoski <shicheng.zhou@my.utsa.edu>
2022-08-09 14:36:35 -0500

    phase 3 Enemy animation

--- script.js
+++ script.js
@@ -74,4 +74,74 @@ window.addEventListener('load', function(){
 
 });
 
+/**@type {HTMLCanvasElement} */
+const canvas2=document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+CANVAS2_WIDTH = canvas2.width = 500;
+CANVAS2_HEIGHT = canvas2.height = 1000;
+const numberOfEnemies = 15;
+const enemiesArray = [];
+
+
+
+let gameFrame = 0;
+
+class Enemy {
+    constructor(){
+        this.image = new Image();
+        this.image.src = 'enemy4.png';
+        this.speed = Math.random()*4-2;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
+        this.width = this.spriteWidth/2.5;
+        this.height = this.spriteHeight/2.5;
+        this.x=Math.random()*(CANVAS2_WIDTH-this.width);
+        this.y=Math.random()*(CANVAS2_HEIGHT-this.height);
+        this.newX=Math.random()*(CANVAS2_WIDTH-this.width);
+        this.newY=Math.random()*(CANVAS2_HEIGHT-this.height);
+        this.frame = 0;
+        this.flapspeed =Math.floor(Math.random()*3+1);
+        this.interval = Math.floor(Math.random()*200+50);
+        
+    }
+
+    update(){
+        //this.x = 0;
+        //this.y = 0;
+        if (gameFrame%this.interval ===0){
+            this.newX=Math.random()*(CANVAS2_WIDTH-this.width);
+            this.newY=Math.random()*(CANVAS2_HEIGHT-this.height);
+        }
+        let dx = this.x -this.newX;
+        let dy = this.y -this.newY;
+
+        this.x -= dx/70;
+        this.y -= dy/70;
+        if(this.x + this.width < 0) this.x =canvas2.width;
+        if(gameFrame%this.flapspeed === 0){
+            this.frame >4? this.frame = 0: this.frame++;
+        } 
+    }
+    draw(){
+        
+        ctx2.drawImage(this.image, this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,
+            this.x,this.y,this.width,this.height);
+    }
+}
+
+for(let i=0; i<numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
+
+function animate2(){
+    ctx2.fillStyle = 'white';
+    ctx2.clearRect(0,0,CANVAS2_WIDTH,CANVAS2_HEIGHT);
+    enemiesArray.forEach(enemy=> {
+        enemy.update();
+        enemy.draw();
+    })
+    gameFrame++;
+    requestAnimationFrame(animate2);
+}
+animate2();
 
